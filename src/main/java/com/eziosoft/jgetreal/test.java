ef806816e36cc8b6fd3c79b81ab5ff727d037fed
EzioisAwesome56 <jedijon567@gmail.com>
2021-12-16 00:38:14 -0500

    ok fine intellij you win

--- src/main/java/com/eziosoft/jgetreal/test.java
+++ src/main/java/com/eziosoft/jgetreal/test.java
@@ -18,39 +18,50 @@ public class test {
         byte[] hec = Files.readAllBytes(Paths.get(f.getAbsolutePath()));
         byte[] test = new byte[0];
         File out = null;
-        if (args[0].equals("static")){
-            test = Caption.captionImage(hec, args[2]);
-            out = new File("dank.png");
-        } else if (args[0].equals("gif")){
-            test = GifCaptioner.CaptionGif(hec, args[2]);
-            out = new File("dank.gif");
-        } else if(args[0].equals("fuzz")){
-            test = ImageFuzzer.FuzzImage(hec);
-            out = new File("fuz.png");
-        } else if (args[0].equals("sort")){
-            test = ImageColorSorter.SortColorsOfImage(hec);
-            out = new File("sort.png");
-        } else if (args[0].equals("giffuzz")){
-            test = GifFuzzer.FuzzGif(hec);
-            out = new File("dank.gif");
-        } else if (args[0].equals("spin")){
-            test = Spin.spinRaster(hec);
-            out = new File("dank.gif");
-        } else if (args[0].equals("gifspin")){
-            test = GifSpin.spinGif(hec);
-            out = new File("dank.gif");
-        } else if (args[0].equals("hypercam")){
-            test = Hypercam2.Unregister(hec);
-            out = new File("dank.png");
-        } else if (args[0].equals("gifhypercam")){
-            test = GifHypercam2.UnregisterGif(hec);
-            out = new File("dank.gif");
-        } else if (args[0].equals("gifcrap")){
-            test = GifUtils.CrapifyGif(hec);
-            out = new File("dank.gif");
-        }else {
-            System.err.println("Error: first argument must either be static or gif");
-            System.exit(-1);
+        switch (args[0]) {
+            case "static":
+                test = Caption.captionImage(hec, args[2]);
+                out = new File("dank.png");
+                break;
+            case "gif":
+                test = GifCaptioner.CaptionGif(hec, args[2]);
+                out = new File("dank.gif");
+                break;
+            case "fuzz":
+                test = ImageFuzzer.FuzzImage(hec);
+                out = new File("fuz.png");
+                break;
+            case "sort":
+                test = ImageColorSorter.SortColorsOfImage(hec);
+                out = new File("sort.png");
+                break;
+            case "giffuzz":
+                test = GifFuzzer.FuzzGif(hec);
+                out = new File("dank.gif");
+                break;
+            case "spin":
+                test = Spin.spinRaster(hec);
+                out = new File("dank.gif");
+                break;
+            case "gifspin":
+                test = GifSpin.spinGif(hec);
+                out = new File("dank.gif");
+                break;
+            case "hypercam":
+                test = Hypercam2.Unregister(hec);
+                out = new File("dank.png");
+                break;
+            case "gifhypercam":
+                test = GifHypercam2.UnregisterGif(hec);
+                out = new File("dank.gif");
+                break;
+            case "gifcrap":
+                test = GifUtils.CrapifyGif(hec);
+                out = new File("dank.gif");
+                break;
+            default:
+                System.err.println("Error: first argument must either be static or gif");
+                System.exit(-1);
         }
         Files.write(Paths.get(out.getAbsolutePath()), test);
     }
