c858ae05dd7d636fe499ca9cea9e2f5b6d3b373e
EzioisAwesome56 <jedijon567@gmail.com>
2022-01-04 13:16:52 -0500

    default to false to save time

--- src/main/java/com/eziosoft/jgetreal/Objects/ImageEffect.java
+++ src/main/java/com/eziosoft/jgetreal/Objects/ImageEffect.java
@@ -7,21 +7,18 @@ public abstract class ImageEffect {
     /**
      * effect name
      */
-    private String name;
+    public String name;
     /**
      * does it need a caption?
      */
-    private boolean needscaption;
-    /**
-     * caption text (may be optional/not required
-     */
-    private String captionText;
+    public boolean needscaption = false;
 
     /**
      * runs your image effect, override this to add new effects to the easy list
      * @param input image to process
+     * @param caption caption text if required
      * @return processed image as an effect result
      * @throws IOException if something goes kaboom
      */
-    public abstract EffectResult runImageEffect(byte[] input) throws IOException;
+    public abstract EffectResult runImageEffect(byte[] input, String... caption) throws IOException;
 }
