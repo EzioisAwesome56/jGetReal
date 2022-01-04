ced28926ece8ff4b5df44fb9793e38773e82b3f4
EzioisAwesome56 <jedijon567@gmail.com>
2022-01-04 13:14:53 -0500

    remove unneeded import statements

--- src/main/java/com/eziosoft/jgetreal/jGetReal.java
+++ src/main/java/com/eziosoft/jgetreal/jGetReal.java
@@ -9,9 +9,7 @@ import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 
 public class jGetReal {
 
