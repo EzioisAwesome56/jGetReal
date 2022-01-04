9cd95aa4d85bb29cee0d4df3e62bba28e482dcba
EzioisAwesome56 <jedijon567@gmail.com>
2022-01-04 14:17:10 -0500

    redo how the cli works; fuck the original implimentation

--- src/main/java/com/eziosoft/jgetreal/jGetReal.java
+++ src/main/java/com/eziosoft/jgetreal/jGetReal.java
@@ -10,11 +10,12 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.HashMap;
+import java.util.Map;
 
 public class jGetReal {
 
     /**
-     * only important for the built-in gui; used to hold all possible effects
+     * only important for the built-in gui and cli; used to hold all possible effects
      */
     public static HashMap<String,  ImageEffect> effects = new HashMap<>();
 
@@ -24,6 +25,20 @@ public class jGetReal {
      * @throws IOException if something explodes
      */
     public static void main(String[] args) throws IOException {
+        System.out.println("Initializing, please wait...");
+        AddEffect(new Bandicam());
+        AddEffect(new Caption());
+        AddEffect(new Funky());
+        AddEffect(new Hypercam2());
+        AddEffect(new IFunny());
+        AddEffect(new ImageColorSorter());
+        AddEffect(new ImageFuzzer());
+        AddEffect(new Invert());
+        AddEffect(new Jpeg());
+        AddEffect(new ShutterStock());
+        AddEffect(new Spin());
+        AddEffect(new TouhouLook());
+        AddEffect(new WhoDid());
         if (args.length < 1) {
             EffectGUI.StartGUI();
         } else {
@@ -32,52 +47,41 @@ public class jGetReal {
             byte[] hec = Files.readAllBytes(Paths.get(f.getAbsolutePath()));
             EffectResult test = null;
             File out = null;
-            switch (args[0]) {
-                case "bandicam":
-                    test = Bandicam.Watermark(hec);
-                    break;
-                case "caption":
-                    test = Caption.applyCaption(hec, args[1]);
-                    break;
-                case "funky":
-                    test = Funky.Watermark(hec);
-                    break;
-                case "hypercam2":
-                    test = Hypercam2.Watermark(hec);
-                    break;
-                case "ifunny":
-                    test = IFunny.Watermark(hec);
-                    break;
-                case "fuzz":
-                    test = ImageFuzzer.Fuzz(hec);
-                    break;
-                case "invert":
-                    test = Invert.Invert(hec);
-                    break;
-                case "shutterstock":
-                    test = ShutterStock.Watermark(hec);
-                    break;
-                case "spin":
-                    test = Spin.spinImage(hec);
-                    break;
-                case "touhoulook":
-                    test = TouhouLook.Watermark(hec);
-                    break;
-                case "whodidthis":
-                    test = WhoDid.Meme(hec);
-                    break;
-                case "jpeg":
-                    test = Jpeg.Crapify(hec);
-                    break;
-                case "sort":
-                    test = ImageColorSorter.Sort(hec);
-                    break;
-                default:
-                    System.err.println("Error: please provide valid effect type!");
-                    System.exit(-1);
+            if (effects.containsKey(args[0])){
+                ImageEffect eff = effects.get(args[0]);
+                if (eff.needscaption){
+                    if (args.length < 3) {
+                        System.err.println("Error: you need to provide a caption as the third argument for this effect!");
+                        System.exit(-1);
+                    }
+                    test = eff.runImageEffect(hec, args[3]);
+                } else {
+                    test = eff.runImageEffect(hec);
+                }
+            } else {
+                System.err.println("Error: the effect '" + args[0] + "' is not valid");
+                StringBuilder b = new StringBuilder();
+                for (Map.Entry<String, ImageEffect> fuck : effects.entrySet()){
+                    b.append(fuck.getKey()).append(", ");
+                }
+                System.err.println("Valid effects are: " + b.toString());
+                System.exit(-1);
             }
             out = new File("dank." + test.getFiletype());
             Files.write(Paths.get(out.getAbsolutePath()), test.getImage());
         }
+    }
+
+    /**
+     * add new effect to the image effect map
+     * @param effect effect to add
+     * @throws IOException if the effect already exists
+     */
+    public static void AddEffect(ImageEffect effect) throws IOException{
+        if (effects.containsKey(effect.name)){
+            throw new IOException("Effect with the same name already exists!");
+        }
+        effects.put(effect.name, effect);
+        System.out.println("Registered effect " + effect.name);
     }
 }
